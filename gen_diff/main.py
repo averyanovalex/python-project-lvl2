c322685c1fe0814c7f2787e2250f16f53b6e90bc
Alexey Averyanov <averyanovalexey@gmail.com>
2022-02-10 23:43:54 +0600

    add input argument format, refact entry script

--- gen_diff/main.py
+++ gen_diff/main.py
@@ -1,22 +1,12 @@
 """GenDiff main module."""
 
-import argparse
 
-
-def arguments_parser() -> argparse.ArgumentParser:
+def run(*args: str) -> None:
     """
-    Create arguments parser an return it.
+    Entrypoint.
 
-    Returns:
-        argparse.ArgumentParser
+    Args:
+        args: list of input arguments.
+
     """
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file', metavar='first_file', type=str)
-    parser.add_argument('second_file', metavar='second_file', type=str)
-    return parser
-
-
-def run() -> None:
-    """Entrypoint."""
-    args = arguments_parser().parse_args()
-    print(args)
+    print('Testing message: coming soon')
--- gen_diff/scripts/gen_diff.py
+++ gen_diff/scripts/gen_diff.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 """GenDiff entrypoint."""
 
+import argparse
+
 from gen_diff.main import run
 
 
 def main() -> None:
     """Entrypoint function."""
-    run()
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', metavar='first_file', type=str)
+    parser.add_argument('second_file', metavar='second_file', type=str)
+    parser.add_argument(
+        '-f',
+        '--format',
+        metavar='FORMAT',
+        help='set format of output',
+    )
+
+    args = parser.parse_args()
+
+    run(args)
 
 
 if __name__ == '__main__':
