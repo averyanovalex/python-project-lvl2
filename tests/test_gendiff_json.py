3abbb15b3ca7ce76d94d6a4308e2365ef5c1ccef
averyanovalex <55646327+averyanovalex@users.noreply.github.com>
2022-02-20 13:32:57 +0600

    Setup CI contur (#2)

--- tests/test_gendiff_json.py
+++ tests/test_gendiff_json.py
@@ -7,66 +7,24 @@ file2_path = 'tests/fixtures/file2.json'
 file_empty_path = 'tests/fixtures/file_empty.json'
 
 
-def test_gendiff_json_both_files_full() -> None:
-    """
-    Tests generate_diff when arguments are json files.
+def test_gendiff_json() -> None:
+    """Tests generate_diff when both arguments are json files."""
+    # both files are full and different
+    with open('tests/fixtures/json_both_full.txt') as file1:
+        estimated_result = file1.read()
+    assert generate_diff(file1_path, file2_path) == estimated_result
 
-    Both files is full.
-    """
-    estimated_result = """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
+    # fisrt file is empty, second file is full
+    with open('tests/fixtures/json_one_empty.txt') as file2:
+        estimated_result = file2.read()
+    assert generate_diff(file_empty_path, file2_path) == estimated_result
 
-    real_result = generate_diff(file1_path, file2_path)
-    assert real_result == estimated_result
+    # both files is equal
+    with open('tests/fixtures/json_both_equal.txt') as file3:
+        estimated_result = file3.read()
+    assert generate_diff(file1_path, file1_path) == estimated_result
 
-
-def test_gendiff_json_one_file_empty() -> None:
-    """
-    Tests generate_diff when arguments are json files.
-
-    Fisrt file is empty, second file is full.
-    """
-    estimated_result = """{
-  + host: hexlet.io
-  + timeout: 20
-  + verbose: true
-}"""
-
-    real_result = generate_diff(file_empty_path, file2_path)
-    assert real_result == estimated_result
-
-
-def test_gendiff_json_both_files_equal() -> None:
-    """
-    Tests generate_diff when arguments are json files.
-
-    Both files is equal.
-    """
-    estimated_result = """{
-    follow: false
-    host: hexlet.io
-    proxy: 123.234.53.22
-    timeout: 50
-}"""
-
-    real_result = generate_diff(file1_path, file1_path)
-    assert real_result == estimated_result
-
-
-def test_gendiff_json_both_file_empty() -> None:
-    """
-    Tests generate_diff when arguments are json files.
-
-    Both file are empty.
-    """
-    estimated_result = """{
-}"""
-
-    real_result = generate_diff(file_empty_path, file_empty_path)
-    assert real_result == estimated_result
+    # both file are empty
+    with open('tests/fixtures/json_both_empty.txt') as file4:
+        estimated_result = file4.read()
+    assert generate_diff(file_empty_path, file_empty_path) == estimated_result
