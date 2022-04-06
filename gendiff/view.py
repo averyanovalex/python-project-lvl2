d54a52779b3c79777804437d6876af6ffb4af9f5
Alexey Averyanov <averyanovalexey@gmail.com>
2022-04-06 07:55:51 +0600

    finish refact view.py

--- gendiff/view.py
+++ gendiff/view.py
@@ -6,25 +6,25 @@ def generate_view(difference: list) -> str:
     Generate view for difference.
 
     Difference between two files has an internal representation.
-    This function generate visual representation for user.
+    This function generate visual representation for user (text view).
 
     Args:
-        difference: difference between two files
+        difference: difference between two files (internal representation)
 
     Returns:
         str
     """
-    return build_view_for_children(difference)
+    return build_view_for_keys_with_differences(difference)
 
 
-def build_view_for_children(nodes: list, indent: str = '') -> str:
+def build_view_for_keys_with_differences(nodes: list, indent: str = '') -> str:
     """
-    Build view for list of difference nodes.
+    Build view for list of keys (nodes) with differences.
 
     Function called recursively.
 
     Args:
-        nodes: list of difference nodes
+        nodes: list keys with differences
         indent: indent for current depth's level
 
     Returns:
@@ -32,11 +32,7 @@ def build_view_for_children(nodes: list, indent: str = '') -> str:
     """
     result_view = '{\n'
     for node in nodes:
-        node_view = build_view_for_node(
-            node.get('value'),
-            node.get('children'),
-            indent,
-        )
+        node_view = build_view_for_node(node, indent)
 
         key = node.get('key')
         marked_indent = get_marked_indent(node.get('diff_type'), indent)
@@ -45,26 +41,58 @@ def build_view_for_children(nodes: list, indent: str = '') -> str:
     return f'{result_view}{indent}}}'
 
 
-def build_view_for_node(value, children, indent):
+def build_view_for_node(node, indent):
     """
     Build view for difference node.
 
     Args:
-        value: node's value
-        children: children
+        node: key with  difference
         indent: indent for current depth's level
 
     Returns:
         str
     """
+    value = node.get('value')
+    children = node.get('children')
+
     indent_next_level = f'{indent}    '
     if isinstance(value, dict):
-        node_view = build_view_for_value(value, indent_next_level)
+        node_view = build_view_for_dict_value(value, indent_next_level)
     elif children is not None:
-        node_view = build_view_for_children(children, indent_next_level)
+        node_view = build_view_for_keys_with_differences(
+            children,
+            indent_next_level,
+        )
     else:
         node_view = str(value)
     return node_view
+
+
+def build_view_for_dict_value(node_value: dict, indent) -> str:
+    """
+    Build view for dict value.
+
+    Value with 'dict' type  output 'as is'.
+    Function called recursively.
+
+    Args:
+        node_value: value with 'dict' type
+        indent: indent for current depth's level
+
+    Returns:
+        str
+    """
+    result_view = '{\n'
+    for key, value in node_value.items():
+        indent_next_level = f'{indent}    '
+
+        if isinstance(value, dict):
+            node_view = build_view_for_dict_value(value, indent_next_level)
+        else:
+            node_view = str(value)
+        result_view = f'{result_view}{indent_next_level}{key}: {node_view}\n'
+
+    return f'{result_view}{indent}}}'
 
 
 def get_marked_indent(diff_type: str, indent: str) -> str:
@@ -86,33 +114,6 @@ def get_marked_indent(diff_type: str, indent: str) -> str:
         indent_suffix = '    '
 
     return f'{indent}{indent_suffix}'
-
-
-def build_view_for_value(value_node: dict, indent) -> str:
-    """
-    Build view for dict value.
-
-    Value with 'dict' type  output 'as is'.
-    Function called recursively.
-
-    Args:
-        value_node: value with 'dict' type
-        indent: indent for current depth's level
-
-    Returns:
-        str
-    """
-    result_view = '{\n'
-    for key, value in value_node.items():
-        indent_next_level = f'{indent}    '
-
-        if isinstance(value, dict):
-            node_view = build_view_for_value(value, indent_next_level)
-        else:
-            node_view = str(value)
-        result_view = f'{result_view}{indent_next_level}{key}: {node_view}\n'
-
-    return f'{result_view}{indent}}}'
 
 
 __all__ = ['generate_view']
